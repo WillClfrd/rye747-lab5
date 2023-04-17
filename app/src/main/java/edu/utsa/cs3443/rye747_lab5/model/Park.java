788c4fd6cf81f13508405d14b29d84bc0eb5ed5a
William Clifford <w.big.red.dog@gmail.com>
2023-04-17 13:41:40 -0500

    Finished Javadoc comments for Park model class

--- app/src/main/java/edu/utsa/cs3443/rye747_lab5/model/Park.java
+++ app/src/main/java/edu/utsa/cs3443/rye747_lab5/model/Park.java
@@ -1,10 +1,7 @@
 package edu.utsa.cs3443.rye747_lab5.model;
-
-import android.app.Activity;
 import android.content.Context;
 import android.content.res.AssetManager;
 import android.util.Log;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -12,31 +9,39 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-//William Clifford rye747
-/*The Park class is used to construct Park objects
- * Park objects have a name stored as a String, a maximum capacity stored as an int, and a list of Dinosaurs in the park stored as an array of Dinosaur objects
- * */
-public class Park {
- private String name;
- private ArrayList<Zone> zones;
- 
- /*The Park constructor takes a String in as the name for the park and an int as the maximum capacity for the park
-  * A park object is constructed using these parameters
+/**
+ * This class represents Park objects.
+ * The Park class maintains a Park object name attribute as well as an ArrayList of Zone objects.
+ *
+ * @author William Clifford (rye747)
+ * UTSA CS 3443 - Lab 5
+ * Spring 2023
  */
- public Park(String pName) {
-	 name = pName;
-     zones = new ArrayList<Zone>();
- }
- 
- /*toString formats the park information into a string by calling the getName method for the park and the Dinosaur toString method for each item in the Dinosaurs array
-  * This formatted array is then returned
-  * */
- public String toString() {
-	 String parkStr = "Welcome to " + getName() + "!\n- - - - - - - - - - - - -\n";
-	 return parkStr;
- }
+public class Park {
+     private String name;
+     private ArrayList<Zone> zones;
 
- public void loadZones(AssetManager manager, Context context, boolean isInitialRead){
+    /**
+     * Constructs a Park object with the specified value assigned to the corresponding Park object attributes.
+     * Initializes the Park object zones ArrayList.
+     *
+     * @param pName the value specified for assignment to the Park object name attribute
+     */
+    public Park(String pName) {
+         name = pName;
+         zones = new ArrayList<Zone>();
+     }
+
+    /**
+     * Parses the contents of the zones.csv, types.csv, and dinos.csv asset files.
+     * Using data read in from the zones.csv asset file Zone objects are added to the Park zones ArrayList attribute.
+     * Using data read in from the types.csv and dinos.csv asset files Dinosaur objects are added to the corresponding Zone object dinos ArrayList attribute.
+     *
+     * @param manager the AssetManager used to access the zones.csv, types.csv, and dinos.csv asset files
+     * @param context the context used to open file input and output streams
+     * @param isInitialRead a boolean value used to determine whether the dinos.csv file should be accessed with the AssetManager manager or not
+     */
+    public void loadZones(AssetManager manager, Context context, boolean isInitialRead){
      ArrayList<String> dinoTypes = new ArrayList<String>();
      boolean[] diets = new boolean[0];
      int dietsSize = 0;
@@ -143,9 +148,16 @@ public class Park {
      catch(IOException e){
          throw new RuntimeException(e);
      }
- }
+    }
 
- private Dinosaur createDinosaur(String name, String type){
+    /**
+     * Returns a Dinosaur object created with the correct Dinosaur subclass based on the data read in from the input file.
+     *
+     * @param name the String value to be passed in as the name parameter for the Dinosaur subclass constructor
+     * @param type used to determine the correct Dinosaur subclass constructor to call for construction of the newDino object
+     * @return a Dinosaur object created with the correct subclass constructor based on the type parameter
+     */
+    private Dinosaur createDinosaur(String name, String type){
      int i;
      Dinosaur newDino;
 
@@ -181,27 +193,51 @@ public class Park {
      }
 
      return newDino;
- }
+    }
 
- //getName is used to return the name of the park as a String
- public String getName() {
-	 return name;
- }
+    /**
+     * Returns the String value of the Park object name attribute.
+     *
+     * @return the name attribute of the Park object
+     */
+    public String getName() {
+     return name;
+    }
 
- public Zone getZone(int index){
+    /**
+     * Returns the Zone object in the Park zones ArrayList attribute corresponding to the specified index.
+     *
+     * @param index the specified index in the Park zones ArrayList attribute of the desired return object
+     * @return the Zone object located at the specified index of the Park zones Arraylist attribute
+     */
+    public Zone getZone(int index){
      return zones.get(index);
- }
+    }
 
- public int getZonesSize(){
+    /**
+     * Returns the integer value of the size of the Park object zones ArrayList.
+     *
+     * @return the count of the Park zones ArrayList attribute elements
+     */
+    public int getZonesSize(){
      return this.zones.size();
- }
- 
- //setName is used to set the park object name to a new String that is passed in as a parameter to setName
- public void setName(String newName) {
-	 name = newName;
- }
+    }
 
- public void addZone(Zone zone){
+    /**
+     * Sets the value of the Park object name attribute to the value specified by newName
+     *
+     * @param newName the value to be assigned to the Park object name attribute
+     */
+    public void setName(String newName) {
+     name = newName;
+    }
+
+    /**
+     * Adds the specified Zone object to the Park object zones ArrayList attribute.
+     *
+     * @param zone the specified Zone object to be added to the Park object zones ArrayList attribute
+     */
+    public void addZone(Zone zone){
      zones.add(zone);
- }
+    }
 }
